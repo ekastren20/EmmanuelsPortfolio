51c8d8864284b9a431c1cf3c11457cb8cc120524
Emmanuel Kastrenakes <ekastren@yahoo.com>
2022-08-11 08:34:42 -0400

    carousel

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,4 @@
+
 document.querySelectorAll(".carousel").forEach(carousel => {
     const items = carousel.querySelectorAll(".carousel__item");
     const buttonsHtml = Array.from(items, () => {
@@ -5,22 +6,28 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     });
 
     carousel.insertAdjacentHTML("beforeend", `
-    <div class="carousel_ _nav">
-        ${buttonsHtml.join("")}    
-    </div>
-    `);
- 
-    const buttons = carousel.querySelectorAll(".carousel__buttons");
+        <div class="carousel__nav">
+            ${ buttonsHtml.join("") }
+        </div>
+        `);
+
+    const buttons = carousel.querySelectorAll(".carousel__button");
 
     buttons.forEach((button, i) => {
         button.addEventListener("click", () => {
-            // un-select all the items
-            items.forEach(item => item.classList.remove("carousel__item--selected"));
-            buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+        //un-select all the items
+         items.forEach(item => item.classList.remove("carousel__item--selected"));
+         buttons.forEach(button => button.classList.remove("carousel__button--selected"));
 
-            items[i].classList.add("carousel__item--selected");
-            button.classList.add("carousel__button--selected");   
-
+         items[i].classList.add("carousel__item--selected");
+         button.classList.add("carousel__button--selected");
         });
     });
+//Select the first item on page load
+    items[0].classList.add("carousel__item--selected");
+    buttons[0].classList.add("carousel__button--selected");
 });
+
+
+
+
